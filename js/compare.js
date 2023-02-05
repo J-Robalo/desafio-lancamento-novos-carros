b2d403c8f31a1142ecb720db69b36917daf71342
João Robalo <joao27of@gmail.com>
2023-02-04 23:39:16 -0300

    Fazendo ajustes no JavaScript do modal

--- js/compare.js
+++ js/compare.js
@@ -1,10 +1,11 @@
 function compareCars() {
   const items = document.querySelectorAll("input[type=checkbox]");
-
+  console.log("teste");
   let checkedItems = [];
 
   for (let i = 0; i < items.length; i++) {
     if (items[i].checked) {
+      console.log("items", checkedItems);
       checkedItems.push(items[i]);
     }
   }
@@ -12,3 +13,16 @@ function compareCars() {
   if (checkedItems.length >= 2) {
   }
 }
+
+const abrirModal = () => {
+  const modal = document.getElementById("modal");
+
+  modal.classList.add("abrir");
+  compareCars();
+
+  modal.addEventListener("click", (event) => {
+    if (event.target.id == "fechar" || event.target.id == "modal") {
+      modal.classList.remove("abrir");
+    }
+  });
+};
